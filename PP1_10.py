d8fdac5fcdab9fd4b573f1ea65dd9efc5d2d157c
Helen-Lin-ICD201-01 <helen.lin@student.tdsb.on.ca>
2024-10-02 13:41:21 +0000

    jungkook

--- PP1_10.py
+++ PP1_10.py
@@ -1,24 +1,43 @@
+'''
+  Lesson: Math Library 
+  Author: Helen Lin
+  Date Created: October 2, 2024
+  Date Last Modified: October 2, 2024
+'''
 
-
+import math
 def q1(): 
   #Write Assignment code here
-
+  num = input("Input a number: ")
+  num = float(num)
+  print(math.sqrt(num))
 
 def q2(): 
   #Write Assignment code here
-
+  num = input("Input a number: ")
+  num = int(num)
+  print(math.isqrt(num))
 
 def q3(): 
   #Write Assignment code here
-
+  num = input("Input a number: ")
+  num = float(num)
+  print(math.floor(num))
 
 def q4(): 
   #Write Assignment code here
-
+  num = input("Input a number: ")
+  num = float(num)
+  print(math.ceil(num))
 
 def q5(): 
   #Write Assignment code here
-
+  num1 = input("Input a number: ")
+  num2 = input("Input another number: ")
+  num1 = float(num1)
+  num2 = float(num2)
+  num = (num1 * num2) / 2 
+  print(math.floor(num))
 
 
 #Do not alter the following code
